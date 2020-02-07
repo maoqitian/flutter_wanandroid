d934076154a209fa4a4e4466711513e4f3555c1b
maoqitian <maoqitian068@163.com>
2020-02-07 23:25:15 +0800

    web page add collect funtion

--- flutter_wanandroid/lib/views/web_page/controller/navigation_control.dart
+++ flutter_wanandroid/lib/views/web_page/controller/navigation_control.dart
@@ -4,13 +4,16 @@
 /// email: maoqitian068@163.com
 /// des:  web_view_page 页面 标题 控制模块
 import 'package:flutter/material.dart';
+import 'package:flutter_wanandroid/common/application.dart';
 import 'package:flutter_wanandroid/common/constants.dart';
+import 'package:flutter_wanandroid/http/data_utils.dart';
 import 'package:flutter_wanandroid/model/route_page_data.dart';
+import 'package:flutter_wanandroid/routers/routes.dart';
 import 'package:flutter_wanandroid/utils/tool_utils.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 
-class NavigationControls extends StatelessWidget {
+class NavigationControls extends StatefulWidget {
 
   const NavigationControls(this._webViewControllerFuture, this.routePageData)
       : assert(_webViewControllerFuture != null);
@@ -20,14 +23,21 @@ class NavigationControls extends StatelessWidget {
   final RoutePageData routePageData;
 
   @override
+  _NavigationControlsState createState() => _NavigationControlsState();
+}
+
+class _NavigationControlsState extends State<NavigationControls> {
+
+
+  @override
   Widget build(BuildContext context) {
     return FutureBuilder<WebViewController>(
-      future: _webViewControllerFuture,
+      future: widget._webViewControllerFuture,
       builder: (BuildContext context, AsyncSnapshot<WebViewController> snapshot) {
         final bool webViewReady = snapshot.connectionState == ConnectionState.done;
         final WebViewController controller = snapshot.data;
         return Row(
-          children: buildIconButton()
+            children: buildIconButton()
         );
       },
     );
@@ -35,16 +45,47 @@ class NavigationControls extends StatelessWidget {
 
   buildIconButton() {
     List<Widget> list =[];
-    if(Constants.COLLECT_PAGE_TYPE == routePageData.type){
+    if(Constants.COLLECT_PAGE_TYPE == widget.routePageData.type){
       list.add(IconButton(
-        icon: const Icon(Icons.favorite),
-        onPressed:  () async{
-          ToolUtils.showToast(msg: "点击了收藏");
-        },
+          icon: Icon(
+              widget.routePageData.collect
+                  ? Icons.favorite
+                  : Icons.favorite_border,
+              color: widget.routePageData.collect
+                  ? Colors.deepOrange
+                  : Colors.white,
+              size: 25.0),
+          onPressed:  () async{
+          await clickCollection();
+       },
       ));
-    }
+  }
     return list;
   }
+
+   clickCollection() async {
+    if (!dataUtils.hasLogin()) {
+      Application.router.navigateTo(context, Routes.login);
+      ToolUtils.showToast(msg: "请先登录");
+      return;
+    }
+    if (widget.routePageData.collect) {
+      //取消收藏
+      await dataUtils.getCancelCollectInnerArticle(widget.routePageData.id);
+      setState(() {
+        widget.routePageData.collect = false;
+      });
+      ToolUtils.showToast(msg: "取消收藏成功");
+    } else {
+      //添加收藏
+      await dataUtils.getCollectInnerArticle(widget.routePageData.id);
+      setState(() {
+        widget.routePageData.collect = true;
+      });
+      ToolUtils.showToast(msg: "收藏成功");
+    }
+  }
+
 }
 
 
